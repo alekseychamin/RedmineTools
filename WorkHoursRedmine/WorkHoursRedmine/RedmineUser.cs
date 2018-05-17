e0334de849158514aeebbabbaf3b7048f60a099f
Aleksey <38381071+alekseychamin@users.noreply.github.com>
2018-05-17 15:35:50 +0300

    create members in class RedmineUser

--- WorkHoursRedmine/WorkHoursRedmine/RedmineUser.cs
+++ WorkHoursRedmine/WorkHoursRedmine/RedmineUser.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Redmine.Net.Api.Types;
 
 namespace WinRedminePlaning
 {
     class RedmineUser
     {
-
+        public User user;
+        public List<TimeEntry> listTimeEntry = new List<TimeEntry>();
+        public List<Issue> listIssue = new List<Issue>();        
     }
 }
