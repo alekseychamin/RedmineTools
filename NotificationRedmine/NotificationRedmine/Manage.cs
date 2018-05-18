9c424afde0864833442f629d755ef396dc935e8f
Aleksey <38381071+alekseychamin@users.noreply.github.com>
2018-05-18 10:30:37 +0300

    add in GetUserIssue filters

--- NotificationRedmine/NotificationRedmine/Manage.cs
+++ NotificationRedmine/NotificationRedmine/Manage.cs
@@ -1,6 +1,8 @@
 ﻿using Redmine.Net.Api;
+using Redmine.Net.Api.Types;
 using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +12,7 @@ namespace NotificationRedmine
     class Manage
     {
         List<UserRedmine> listUserRedmine = new List<UserRedmine>();
-        RedmineManager redmineManager;
+        RedmineManager redmineManager = null;
 
         public Manage(string host, string key)
         {
@@ -23,5 +25,30 @@ namespace NotificationRedmine
                 Console.WriteLine("Could not connect to redmine host! " + ex.Message);
             }
         }
+
+        public void GetUserIssue()
+        {
+            if (redmineManager != null)
+            {
+                NameValueCollection param = new NameValueCollection { {"Id", "*" } };
+                foreach (var user in redmineManager.GetObjects<User>(param))
+                {
+                    UserRedmine userRedmine = new UserRedmine();
+                    userRedmine.Value = user;
+                    listUserRedmine.Add(userRedmine);
+                    Console.WriteLine("User = {0}", userRedmine.Value.LastName.ToString());
+                    
+                    param = new NameValueCollection { { "assigned_to_id", user.Id.ToString()}, { "status_id", "open" } };
+                    foreach (var issue in redmineManager.GetObjects<Issue>(param))
+                    {
+                        userRedmine.ListIssue.Add(issue);
+                        Console.WriteLine("Issue = {0}", issue.Subject.ToString());                                                                        
+                    }
+
+                }
+            }
+        }
+
+        
     }
 }
--- NotificationRedmine/NotificationRedmine/Program.cs
+++ NotificationRedmine/NotificationRedmine/Program.cs
@@ -10,7 +10,8 @@ namespace NotificationRedmine
     {
         static void Main(string[] args)
         {
-            Manage manage = new Manage("", "");
+            Manage manage = new Manage("188.242.201.77", "70b1a875928636d8d3895248309344ea2bca6a5f");
+            manage.GetUserIssue();
 
             Console.ReadKey();
         }
