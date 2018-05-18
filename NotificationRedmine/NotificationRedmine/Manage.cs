6284d69ad87911e44949597136857732651a0205
Aleksey <38381071+alekseychamin@users.noreply.github.com>
2018-05-18 09:23:23 +0300

    add new clasd manage

---
+++ NotificationRedmine/NotificationRedmine/Manage.cs
@@ -0,0 +1,27 @@
+﻿using Redmine.Net.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotificationRedmine
+{
+    class Manage
+    {
+        List<UserRedmine> listUserRedmine = new List<UserRedmine>();
+        RedmineManager redmineManager;
+
+        public Manage(string host, string key)
+        {
+            try
+            {
+                redmineManager = new RedmineManager(host, key);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Could not connect to redmine host! " + ex.Message);
+            }
+        }
+    }
+}
