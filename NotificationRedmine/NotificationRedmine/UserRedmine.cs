9bb5411583774a9a11cb38f715b3f97bc98ed1a4
Aleksey <38381071+alekseychamin@users.noreply.github.com>
2018-05-18 09:12:12 +0300

    add class UserRedmine

---
+++ NotificationRedmine/NotificationRedmine/UserRedmine.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Redmine.Net.Api.Types;
+
+namespace NotificationRedmine
+{
+    class UserRedmine
+    {
+        public User Value;
+        public List<Issue> ListIssue = new List<Issue>();        
+    }
+}
