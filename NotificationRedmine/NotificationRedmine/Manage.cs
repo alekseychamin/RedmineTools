7c6d4a087d0a72b0e47feb22b1d6b142f9fcc260
Aleksey <38381071+alekseychamin@users.noreply.github.com>
2018-05-20 23:24:38 +0300

    final comment

--- NotificationRedmine/NotificationRedmine/Manage.cs
+++ NotificationRedmine/NotificationRedmine/Manage.cs
@@ -17,12 +17,23 @@ namespace NotificationRedmine
 
         struct DayTimeStart
         {
-            public string dayOfWeek;
+            public DayOfWeek dayOfWeek;
             public string timeStart;
         }
 
+        DayTimeStart[] dayTimeStart = new DayTimeStart[3];
+
         public Manage(string host, string key)
         {
+            dayTimeStart[0].dayOfWeek = DayOfWeek.Monday;
+            dayTimeStart[0].timeStart = "9:00";
+
+            dayTimeStart[1].dayOfWeek = DayOfWeek.Wednesday;
+            dayTimeStart[1].timeStart = "9:00";
+
+            dayTimeStart[2].dayOfWeek = DayOfWeek.Friday;
+            dayTimeStart[2].timeStart = "9:00";
+
             try
             {
                 email = new Email();
@@ -34,8 +45,74 @@ namespace NotificationRedmine
             }
         }
         
+        private bool isDayOfWeek(DayOfWeek dayOfWeek, out string timeStart)
+        {
+            bool result = false;
+            timeStart = "";
+
+            foreach (DayTimeStart dayTime in dayTimeStart)
+            {
+                if (dayTime.dayOfWeek == dayOfWeek)
+                {
+                    result = true;
+                    timeStart = dayTime.timeStart;
+                    break;
+                }
+            }
+
+            return result;
+        }
+
+        private DateTime GetDate(DateTime stepDateTime, string timeStart)
+        {                    
+            int year = stepDateTime.Year;
+            int mounth = stepDateTime.Month;
+            int day = stepDateTime.Day;
+
+            string[] timeParts = timeStart.Split(new char[1] { ':' });
+            int hour = int.Parse(timeParts[0]);
+            int minute = int.Parse(timeParts[1]);
+
+            return new DateTime(year, mounth, day, hour, minute, 0);            
+        }
+
+        public TimeSpan GetTimeStart()
+        {
+            bool done = false;
+            string timeStart = "";
+
+            TimeSpan dt = new TimeSpan();
+
+            DateTime stepDateTime = DateTime.Now;
+            DateTime currentDateTime = DateTime.Now;
+
+            while (!done)
+            {                
+                if (isDayOfWeek(stepDateTime.DayOfWeek, out timeStart) && ( (dt = ( GetDate(stepDateTime, timeStart) - currentDateTime)).TotalSeconds > 0) )
+                {                    
+                    done = true;                    
+                }
+                else
+                    stepDateTime = stepDateTime.AddDays(1);
+            }
+
+            Console.WriteLine("near date to start = {0}, dt = {1} hours", GetDate(stepDateTime, timeStart).ToString(), dt.TotalHours);
+
+            return dt;
+        }
+
+        public void MakeNotificationToTime()
+        {
+            GetUserOpenIssue();
+            SetNotificationUser();
+            ShowNotification();
+            SendEmail();
+        }
+
         public void GetUserOpenIssue()
         {
+            listUserRedmine.Clear();
+
             try
             {
                 NameValueCollection param = new NameValueCollection { { "Id", "*" } };
@@ -118,8 +195,9 @@ namespace NotificationRedmine
 
             if (userRedmine.message == "")
             {
-                message = "Уважаемый(ая) " + userRedmine.FullName + "!" + "\n";
-                message += "У вас имеются открытые просроченные задания, которые в случае их выполнения необходимо закрыть или согласовать новую дату завершения : " + "\n";
+                message = "Здравствуйте!" + "\n";
+                message += "Уважаемый(ая) " + userRedmine.FullName + "!" + "\n";
+                message += "У вас есть в системе открытые просроченные задания, которые в случае их выполнения необходимо закрыть (статус - закрыта) или согласовать новую дату завершения : " + "\n";
                 message += "\n";
 
                 userRedmine.message = message;
--- NotificationRedmine/NotificationRedmine/Program.cs
+++ NotificationRedmine/NotificationRedmine/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace NotificationRedmine
@@ -11,12 +12,14 @@ namespace NotificationRedmine
         static void Main(string[] args)
         {
             Manage manage = new Manage("188.242.201.77", "70b1a875928636d8d3895248309344ea2bca6a5f");
-            manage.GetUserOpenIssue();
-            manage.SetNotificationUser();
-            manage.ShowNotification();
-            manage.SendEmail();       
+            TimeSpan dt;
 
-            Console.ReadKey();
+            while (true)
+            {
+                dt = manage.GetTimeStart();
+                Task.Delay(dt).ContinueWith((x) => manage.MakeNotificationToTime());
+                Thread.Sleep(dt);
+            }            
         }
     }
 }
